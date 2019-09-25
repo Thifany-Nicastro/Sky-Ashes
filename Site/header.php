72fca5ad7e5a18eebb41616815746f1696ba9131
Thifany Nicastro <thifanynicastro@hotmail.com>
2019-09-25 10:58:40 -0300

    Add files via upload

---
+++ Site/header.php
@@ -0,0 +1,28 @@
+<header class="mt-3 text-center">
+    <div class="logo mb-3">
+        <img src="star.png" width="100px;">
+    </div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark" id="nav">
+        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#nav-links"
+            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-links">
+            <ul class="navbar-nav mx-auto p-2">
+                <li class="nav-item">
+                    <a class="nav-link hvrcenter" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link hvrcenter" href="download.php">Download</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link hvrcenter" href="equipe.php">Equipe</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link hvrcenter" href="https://github.com/Thifany-Nicastro/Sky-Ashes">Repositório</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
