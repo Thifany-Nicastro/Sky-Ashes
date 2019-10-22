d1d0d2ed3cdcff7bc1d49232940cdf97faeef282
Thifany Nicastro <thifanynicastro@hotmail.com>
2019-10-22 08:18:38 -0300

    Update download.php

--- Site/download.php
+++ Site/download.php
@@ -11,6 +11,8 @@
 
     <!-- Custom styles for this template -->
     <link href="css/navbar.css" rel="stylesheet">
+    
+    <link rel="shortcut icon" href="img/star.ico" />
 
     <title>Download</title>
 </head>
