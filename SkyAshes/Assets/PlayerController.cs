37032feddb49fb5b1e5649af412a72b968ad11de
Thifany Nicastro <thifanynicastro@hotmail.com>
2019-11-23 20:12:27 -0300

    Cenário

---
+++ SkyAshes/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 7f);
+        }
+    }
+}
