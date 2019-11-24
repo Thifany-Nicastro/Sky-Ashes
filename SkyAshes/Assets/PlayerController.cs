6284f0f96b9a9220ab6476e406fa2017898841e2
Thifany Nicastro <thifanynicastro@hotmail.com>
2019-11-24 12:33:35 -0300

    Animações

--- SkyAshes/Assets/PlayerController.cs
+++ SkyAshes/Assets/PlayerController.cs
@@ -13,10 +13,12 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
