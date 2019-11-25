a66adc3b0f390ce96acf82b2ba44c8e423d2a285
Thifany Nicastro <thifanynicastro@hotmail.com>
2019-11-25 06:56:23 -0300

    Enemy

---
+++ SkyAshes/Assets/Scripts/Crab.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crab : MonoBehaviour
+{
+    [SerializeField] private float leftCap;
+    [SerializeField] private float rightCap;
+
+    [SerializeField] private float jumpLenght;
+    [SerializeField] private float jumpHeight;
+
+    private bool facingLeft = true;
+
+
+
+}
